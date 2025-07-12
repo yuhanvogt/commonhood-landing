e50e523d8f26729a76872e6e8cf7f101f6f8b4af
Moein Mirkiani <moein.mirkiani@gmail.com>
2025-07-12 15:31:25 +0200

    refactor: improve hero's style

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,16 +3,14 @@
         <img :src="HeroImage" alt="Commonhood" class="w-full h-full object-cover" />
 
         <ClientOnly>
-            <div class="max-w-[300px] mx-auto text-center absolute top-[128px] left-1/2 transform -translate-x-1/2 sm:top-[156px] md:max-w-[460px] md:top-[172px] lg:top-[200px] xl:max-w-[544px] xl:top-[288px] 2xl:max-w-[680px] 2xl:top-[260px]">
-                <h1 class="uppercase text-gray-500 text-xl leading-5 sm:text-3xl sm:leading-7 md:text-4xl md:leading-9 lg:text-5xl xl:text-6xl 2xl:text-7xl">Commonhood is</h1>
+            <div class="max-w-[240px] mx-auto text-center absolute top-[136px] left-1/2 transform -translate-x-1/2 sm:max-w-[320px] sm:top-[156px] md:max-w-[400px] md:top-[188px] lg:max-w-[500px] lg:top-[212px] xl:max-w-[600px] xl:top-[308px] 2xl:max-w-[760px] 2xl:top-[280px]">
+                <h1 class="uppercase text-gray-500 text-2xl leading-6 sm:text-4xl sm:leading-8 md:text-[42px] md:leading-9 lg:text-[52px] lg:leading-[48px] xl:text-[68px] xl:leading-[60px] 2xl:text-[84px] 2xl:leading-[72px]">Commonhood is</h1>
 
                 <swiper-container ref="swiperRef" :init="false">
                     <swiper-slide v-for="(item, index) in swiperItems" :key="index">
-                        <h3 class="uppercase text-xl leading-5 sm:text-3xl sm:leading-7 md:text-4xl md:leading-9 lg:text-5xl xl:text-6xl 2xl:text-7xl">{{ item }}</h3>
+                        <h3 class="uppercase text-2xl leading-6 sm:text-4xl sm:leading-8 md:text-[42px] md:leading-9 lg:text-[52px] lg:leading-[48px] xl:text-[68px] xl:leading-[60px] 2xl:text-[84px] 2xl:leading-[72px]">{{ item }}</h3>
                     </swiper-slide>
                 </swiper-container>
-
-                <p class="text-gray-400 mt-4 text-xs lg:mt-6 lg:text-xl xl:mt-10 2xl:mt-12 2xl:text-2xl">Scroll to discover</p>
             </div>
         </ClientOnly>
     </section>
